cb3654df38d58498b4fd75de9136c2bec0516e04
Trey Schisler <tschisle@users.noreply.github.com>
2019-09-10 03:39:05 -0400

    Latest stable version of mafia
    Added test to see how the RNG acted and a sanity check for the quick fix to help bias it, making it more even at low sample sizes (number of games)
    WILL NEED A CLEARING FUNCTION ASAP (can be combined with endgame function in the later version)

--- mafia1.js
+++ mafia1.js
@@ -1,7 +1,3 @@
-/**
- * A ping pong bot, whenever you send "ping", it replies "pong".
- */
-
 // Import the discord.js module
 const Discord = require('discord.js');
 
@@ -12,6 +8,7 @@ var mafiaso;
 var memNum;
 var mafcount = [6];
 var tempmafcount = [6];
+var rngmafcount = [6];
 var newgameflag = 0;
 var gamecount;
 
@@ -33,12 +30,95 @@ client.on('message', message => {
     // Send "pong" to the same channel
     message.channel.send('pong');
   }
+  
+  if (message.content === '!rng2') { 
+    // tests new rng
+	//message.channel.send('Starting RNG2');
+	
+	for(var x3 = 0; x3 < 6; x3++){
+		tempmafcount[x3] = 0;
+	}
+	//message.channel.send('Starting RNG2');
+	
+	var randRNG2;
+    
+	for(var x2 = 0; x2 < 18; x2++){
+		randRNG2 = Math.floor((Math.random() * 1000) % 6);
+		if(randRNG2 == 0){
+			tempmafcount[0]++;
+		} else if(randRNG2 == 1){
+			tempmafcount[1]++;
+		} else if(randRNG2 == 2){
+			tempmafcount[2]++;
+		} else if(randRNG2 == 3){
+			tempmafcount[3]++;
+		} else if(randRNG2 == 4){
+			tempmafcount[4]++;
+		} else if(randRNG2 == 5){
+			tempmafcount[5]++;
+		} else {
+			message.channel.send('RNG value not within 0-5');
+		}
+	}
+	message.channel.send('' + tempmafcount[0] + '\n' + tempmafcount[1] + '\n' + tempmafcount[2] + '\n' + tempmafcount[3] +'\n' + tempmafcount[4] +'\n' + tempmafcount[5]);
+	//message.channel.send('1 - ' + tempmafcount[0] + '\n2 - ' + tempmafcount[1] + '\n3 - ' + tempmafcount[2] + '\n4 - ' + tempmafcount[3] +'\n5 - ' + tempmafcount[4] +'\n6 - ' + tempmafcount[5]);
+  }
+  
+    if (message.content === '!rng') {
+    // tests old rng
+	//message.channel.send('Starting RNG2');
+	
+	for(var x4 = 0; x4 < 6; x4++){
+		tempmafcount[x4] = 0;
+	}
+	//message.channel.send('Starting RNG2');
+	
+	var randRNG2;
+    
+	for(var x5 = 0; x5 < 18; x5++){
+		for(var x6 = 0; x6 < 6; x6++){
+			rngmafcount[x6] = tempmafcount[x6];
+		}
+		randRNG2 = Math.floor((Math.random() * 1000) % 6);
+		while(rngmafcount[randRNG2] > 0){
+			rngmafcount[randRNG2]= rngmafcount[randRNG2] - 1;
+			randRNG2 = Math.floor((Math.random() * 1000) % 6);
+		}
+		/*
+		if((x5%5)==0){
+			message.channel.send('#' + x5);
+		}
+		*/
+		if(randRNG2 == 0){
+			tempmafcount[0]++;
+		} else if(randRNG2 == 1){
+			tempmafcount[1]++;
+		} else if(randRNG2 == 2){
+			tempmafcount[2]++;
+		} else if(randRNG2 == 3){
+			tempmafcount[3]++;
+		} else if(randRNG2 == 4){
+			tempmafcount[4]++;
+		} else if(randRNG2 == 5){
+			tempmafcount[5]++;
+		} else {
+			message.channel.send('RNG value not within 0-5');
+		}
+	}
+	message.channel.send('' + tempmafcount[0] + '\n' + tempmafcount[1] + '\n' + tempmafcount[2] + '\n' + tempmafcount[3] +'\n' + tempmafcount[4] +'\n' + tempmafcount[5]);
+	//message.channel.send('1 - ' + tempmafcount[0] + '\n2 - ' + tempmafcount[1] + '\n3 - ' + tempmafcount[2] + '\n4 - ' + tempmafcount[3] +'\n5 - ' + tempmafcount[4] +'\n6 - ' + tempmafcount[5]);
+  }
+  
+  
+  
   if (message.content === '!reveal') {
 	   if (currentMafia != '') {
 	   voteChannel.send(currentMafia + ' was the mafiaso!');
 	   currentMafia = '';
 	}
    }
+   
+   
   if (message.content === '!newgame') {
     // Create Mafia Channel
 	if(newgameflag === 0){
@@ -55,9 +135,10 @@ client.on('message', message => {
     totalMafia = mafChannel.members.array().length
 	//To try to even out mafia assignments while keeping some randomness
 	//NOTE: this adds more time as more and more games are played
+	randomMafia = Math.floor((Math.random() * 1000) % totalMafia);	
 	while(tempmafcount[randomMafia] > 0){
-		tempmafcount[randomMafia]--;
-		randomMafia = Math.floor(Math.random() * totalMafia);
+		tempmafcount[randomMafia]= tempmafcount[randomMafia] - 1;
+		randomMafia = Math.floor((Math.random() * 1000) % totalMafia);
 	}
 	for (var i = 0; i < mafChannel.members.array().length; i++) {
 		if (i == randomMafia){
@@ -70,19 +151,6 @@ client.on('message', message => {
     	}
 	
 	}
-    /*
-	for (var i = 0; i < message.guild.channels.array().length; i++) {
-    message.guilds.channels.array()[i].delete();
-	};
-    message.guild.createChannel('Vote','text')
-    .then(console.log)
-    .catch(console.error);
-    message.guild.createChannel('Mafia','voice')
-    .then(console.log)
-    .catch(console.error);*/
-  //if (message.content === '!reset') {
-    // Create Mafia Channel
-  	//resetBot(message.channel);
   }
 });
 
